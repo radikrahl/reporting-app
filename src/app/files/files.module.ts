967ac0c86e5e92ac8806e966f249b403e81ca38e
Andreas Krahl <a.krahl90@gmail.com>
2023-03-14 09:26:42 +0100

    added files module

---
+++ src/app/files/files.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FilesComponent } from "./pages/files.component";
+
+const routes: Routes = [{ path: "files", component: FilesComponent }];
+
+@NgModule({
+  declarations: [FilesComponent],
+
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class FilesModule {}
---
+++ src/app/files/pages/files.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { Page } from "@nativescript/core";
+
+@Component({
+  selector: 'afriknow-files',
+  templateUrl: './files.component.html',
+  styleUrls: ['./files.component.css']
+})
+export class FilesComponent {
+/**
+ *
+ */
+constructor(private page: Page) {
+}
+}
